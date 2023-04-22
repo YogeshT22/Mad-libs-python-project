19d1c0748e8f859ff9340fe4c7ad62d7b88e3fe0
Yogesh T <yogeshoffy@gmail.com>
2023-04-22 15:02:22 +0530

    Added source code "mad libs"

---
+++ mad-libs.py
@@ -0,0 +1,9 @@
+youtuber = input("Enter name of your YT channel: ")
+print(f"Subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous Person: ")
+madlib = f"Computer Programming is {adj}! It makes me so excited all the time because\nI love to {verb1}. Stay happy and {verb2} like you {famous_person}!"
+
+print(madlib)
